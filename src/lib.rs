d308293f5304f154808948b4bec75a9fae441c8a
hargados <hargados@tcd.ie>
2016-03-29 19:49:04 +0100

    init

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+macro_rules! compose {
+    ( $c:expr, ) => { $c };
+    ( $c:expr, . $x:ident $(.$y:ident)* ) => { $x ( compose!($c, $(. $y)* ) ) };
+    ( $x:ident $(. $y:ident)* ) => { |arg| $x ( compose!(arg, $(. $y)* ) ) };
+}
